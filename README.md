a4ab1ab708ae70b73d12694f5bc4a84922a93e6a
Gianluca Luparini <gianlucalup@gmail.com>
2022-07-28 12:17:34 +0200

    Created some folders/files and /src/interface.c specification

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ProgettoSOL-20-21
-Progetto (2020/2021) per il corso di Sistemi Opertativi e Laboratorio dell'Università di Pisa
+Progetto (2020/2021) per il corso di Sistemi Operativi e Laboratorio dell'Università di Pisa
---
+++ src/interface.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+
+/*
+openConnection: opens an AF_UNIX connection to socket 'sockname'. If the connection is not accepted immediately, the client connection is
+		repeated after 'msec' milliseconds. After 'abstime' connection attempts are not possible anymore.
+		Returns (0) if success, (-1) if failure and sets errno.
+*/
+int openConnection(const char* sockname, int msec, const struct timespec abstime){
+	//TODO
+}
+
+
+/*
+closeConnection: closes the AF_UNIX connection to socket 'sockname'.
+		 Returns (0) if success, (-1) if failure and sets errno.
+*/
+int closeConnection(const char* sockname){
+	//TODO
+}
+
+
+/*
+openFile: open or creation request for file 'pathname'. Possible 'flags' are O_CREATE and O_LOCK (it's possible to OR them)
+	  Returns (0) if success, (-1) if failure and sets errno.
+*/
+int openFile(const char* pathname, int flags){
+	//TODO
+}
+
+
+/*
+readFile: reads all the contents in file 'pathname' and returns a pointer to heap memory in 'buf' and the buffer size in 'size'.
+	  Returns (0) if success, (-1) if failure (i.e. 'pathname' doesn't exist) and sets errno.
+*/
+int readFile(const char* pathname, void** buf, size_t* size){
+	//TODO
+}
+
+
+/*
+readNFiles: client request for N files. If server file count is <N or N<=0 then it sends all the files to the client.
+	    Returns (0) if success, (-1) if failure and sets errno.
+*/
+int readNFiles(int N, const char* dirname){
+	//TODO
+}
+
+
+/*
+writeFile: writes the 'pathname' file in the server. Server may expel one file to store 'pathname' file. If this is the case: if 'dirname' is
+	   not NULL, the expelled file from server is saved in 'dirname', else the expelled file is destroyed.
+	   Returns (0) if success, (-1) if failure and sets errno.
+	   
+*/
+int writeFile(const char* pathname, const char* dirname){
+	//TODO
+}
+
+/*
+appendToFile: append to 'pathname' 'size' bytes from buffer 'buf'. The append operation is atomic. Server may expel one file to store 'pathname'
+	      file. If this is the case: if 'dirname' is not NULL, the expelled file from server is saved in 'dirname', else the expelled 
+	      file is destroyed.
+	      Returns (0) if success, (-1) if failure and sets errno. 
+*/
+int appendToFile(const char* pathname, void* buf, size_t size, const char* dirname){
+	//TODO
+}
+
+/*
+lockFile: sets the O_LOCK flag for file 'pathname'. If 'pathname' was opened/created with O_LOCK flag and the request is from the same process
+	  or the file's O_LOCK flag is not setted, then the operation returns success, else the operation waits that the O_LOCK flag is resetted
+	  by the lock owner.
+	  Returns (0) if success, (-1) if failure and sets errno. 
+*/
+int lockFile(const char* pathname){
+	//TODO
+}
+
+/*
+unlockFile: resets the O_LOCK flag for file 'pathname'.
+	    Returns (0) if success (lock owner calls unlockFile), (-1) failure (unlockFile is called by another process) and sets errno.
+*/
+int unlockFile(const char* pathname){
+	//TODO
+}
+
+
+/*
+closeFile: close request of file 'pathname'.
+	   Returns (0) if success, (-1) if failure and sets errno.
+*/
+int closeFile(const char* pathname){
+	//TODO
+}
+
+
+/*
+removeFile: remove file 'pathname' from the server.
+	    Returns (0) if success, (-1) if failure (file is not locked or is locked by another process) and sets errno.
+*/
+int removeFile(const char* pathname){
+	//TODO
+}

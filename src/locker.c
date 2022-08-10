83e80a7073014de78cae682e4e01d5aa665e6e1d
Gianluca Luparini <gianlucalup@gmail.com>
2022-08-10 18:26:37 +0200

    readLock and readUnlock implementation

--- src/locker.c
+++ src/locker.c
@@ -4,13 +4,15 @@
 
 #include <locker.h>
 
+//lock system to manage read/write concurrent operations
+//pthread mutex and condition are used to modify the locker internal state
+
 struct locker{
 	pthread_mutex_t mux;
 	pthread_cond_t cond;
 	bool writerPresence;
 	int readersNumber;
 }
-
 
 locker* lockerInit(){
 	int err;
@@ -38,4 +40,45 @@ locker* lockerInit(){
 	
 	return newLock;
 }
+
+
+int readLock(locker* lock){
+	if(lock == NULL){
+		errno = EINVAL;
+		return -1;
+	}
+	if(pthread_mutex_lock(&(lock->mux)) != 0){
+		return -1;					//fatal error
+	}
+	while(lock->writerPresence){				//waiting for writer/s to finish
+		pthread_cond_wait(&(lock->cond), &(lock->mux));
+	}
+	lock->readersNumber++;					//a reader joined the reader's group
+	if(pthread_mutex_unlock(&(lock->mux)) != 0){
+		return -1;
+	}
+	
+	return 0;
+}
+
+int readUnlock(locker* lock){
+	if(lock == NULL){
+		errno = EINVAL;
+		return -1;
+	}
+	if(pthread_mutex_lock(&(lock->mux)) != 0){
+		return -1;
+	}
+	lock->readersNumber--;					//a reader left the reader's group
+	if(lock->readersNumber == 0){
+		pthread_cond_signal(&(lock->cond));
+	}					
+	if(pthread_mutex_unlock(&(lock->mux)) != 0){
+		return -1;
+	}
+	
+	return 0;
+
+}
+
 
